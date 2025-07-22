7a3094ac4e1969d8d27a81b3f3a8eab99750c66f
BrunoNolasco <brunonolasco2763@gmail.com>
2025-07-22 13:17:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,70 @@
-import os
-import time
-
-def calculadora(num1: float, num2: float, operador: str) -> float:
-    """
-    Usar nan como valor inicial é uma boa prática. 
-    Se o operador fornecido não corresponder a nenhuma das opções válidas (+, -, etc.), a função retornará nan, 
-    sinalizando que o cálculo não pôde ser realizado.
-    """
-    result = float("nan")
-    if operador == '+':
-        result = num1 + num2
-
-    return result
+def soma():
+    try:
+        num1 = int(input("Introduza o primeiro número: "))
+        num2 = int(input("Introduza o segundo número: "))
+    
+        soma = num1 + num2
+        
+        print(f"O resultado da soma é: {soma}")
+    except ValueError:
+        print("Os números devem ser inteiros")
 
 
-if __name__ == "__main__":
-
-    while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        try:
-            print('Calculadora')
-            print('----------------------------------\n')
 
 
-        except ValueError:
-            print('Dados inválidos! -> Tente novamente!')
-            time.sleep(2)
 
-        except ZeroDivisionError:
-            print('Impossível dividir por zero! -> Tente novamente!')
-            time.sleep(2)
 
-    print('\nVolte sempre!\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#import os
+#import time
+#
+#def calculadora(num1: float, num2: float, operador: str) -> float:
+#    """
+#    Usar nan como valor inicial é uma boa prática. 
+#    Se o operador fornecido não corresponder a nenhuma das opções válidas (+, -, etc.), a função retornará nan, 
+#    sinalizando que o cálculo não pôde ser realizado.
+#    """
+#    result = float("nan")
+#    if operador == '+':
+#        result = num1 + num2
+#
+#    return result
+#
+#
+#if __name__ == "__main__":
+#
+#    while True:
+#        os.system('cls' if os.name == 'nt' else 'clear')
+#        try:
+#            print('Calculadora')
+#            print('----------------------------------\n')
+#
+#
+#        except ValueError:
+#            print('Dados inválidos! -> Tente novamente!')
+#            time.sleep(2)
+#
+#        except ZeroDivisionError:
+#            print('Impossível dividir por zero! -> Tente novamente!')
+#            time.sleep(2)
+#
+#    print('\nVolte sempre!\n')
+#
